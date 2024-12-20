f7cdeabfa022cca62c26225e2ac70a6e955c7fd8
Daniel <35966782+danielrm91@users.noreply.github.com>
2024-12-19 20:06:37 -0500

    Add files via upload

---
+++ Exp2/main_daniel.py
@@ -0,0 +1,75 @@
+'''
+this file is based on the work on 
+https://medium.com/@eyyu/coding-ppo-from-scratch-with-pytorch-part-1-4-613dfc1b14c8
+without some functions that are not convenient for the purpose
+'''
+
+import gym
+import gym_kondili
+import sys
+import torch
+
+from arguments_daniel import get_args
+from eval_policy_daniel_iterative import eval_policy
+from ppo_daniel import PPO                     #import this class from ppo
+from network_daniel import RNNactor  #import this class from network
+from network_daniel import RNNcritic #import this class from network
+
+def train(env, hyperparameters, actor_model, critic_model):
+    print(f"Training",flush=True)
+    #Create a model for PPO
+    model = PPO(policy_class=RNNactor, value_class=RNNcritic, env=env, **hyperparameters)
+    
+    #load existing model
+    if actor_model != '' and critic_model != '':
+        print(f"Loading in {actor_model} and {critic_model}...", flush=True)
+        model.actor.load_state_dict(torch.load(actor_model))
+        model.critic.load_state_dict(torch.load(critic_model))
+        print(f"Succesfully loaded.",flush=True)
+    elif actor_model != '' or critic_model != '':
+        print(f"Error: Either specify both actor/critic models or non at all. We don't want to accidentally override anything!")
+        sys.exit(0)
+    else:
+        print(f"Training from scratch.", flush=True)
+    model.learn(total_timesteps = 930_000)
+def test(env, actor_model):
+    print(f"Testing {actor_model}", flush=True)
+    if actor_model =='':
+        print(f"Didn't specify model file. Exiting", flush=True)
+        sys.exit(0)
+    seq_len = 4
+    obs_dim = 15
+    sen_dim = 250
+    actd_dim = 9
+    actc_dim = 8
+    policy = RNNactor(obs_dim, sen_dim, actd_dim, actc_dim, seq_len)
+    policy.load_state_dict(torch.load(actor_model))
+    eval_policy(policy=policy, env=env)
+
+def main(args):
+    hyperparameters = {
+        'timesteps_per_batch':          310,       #225,#75..
+        'seq_len':                      4,        #new hyperparameter
+        'max_timesteps_per_episode':    31,
+        'gamma':                        0.99,       #0.5
+        'n_updates_per_iteration':      5,         #8 #6       
+        'lr':                           1e-4,      #1e-6
+        'clip':                         0.2,       #0.2
+        'ent_init':                     0.01,
+        'max_grad_norm':                0.5,
+        'var' :                         1e-2,      #0.08,0.02
+        'temp':                         1,
+        'num_minibatches' :             5,
+        'lam':                          0.98,
+        'target_kl':                    0.02,  
+    }
+
+    env = gym.make('kondili-v0')
+    if args.mode == 'train':
+        train(env=env, hyperparameters=hyperparameters, actor_model=args.actor_model, critic_model=args.critic_model)
+    else:
+        test(env=env, actor_model=args.actor_model)
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args)
---
+++ Exp2/ppo_daniel.py
@@ -0,0 +1,407 @@
+
+
+'''
+PPO class to train and learn the policy
+'''
+
+import gym
+import gym_kondili
+import time
+import csv
+import numpy as np
+import torch
+import random
+import torch.nn as nn
+from torch.optim import Adam
+from torch.distributions import MultivariateNormal
+from torch.distributions import Categorical
+import torch.nn.functional as F
+
+#OBS:
+#    [H, R1, R2, D, HA, intBC, intAB, impE, P1, P2, tR1, tR2, H,  D,  t ]
+#    [0  1   2   3  4   5      6      7     8   9   10   11   12  13  14]
+
+class PPO:
+    def __init__(self, policy_class, value_class, env, **hyperparameters):
+        self.max_vals_obs = torch.tensor([100,80,50,200,300,300,300,300,200,200,4,4,2,2,30], dtype=torch.float32)
+        self._init_hyperparameters(hyperparameters)
+        self.env = env
+        self.obs_dim =      15
+        self.sen_dim =      250      #Encoder output
+        self.out_ac1_dim =  9        #Discrete actor, Action 9 is idle state
+        self.out_ac2_dim =  8        #Continuous actor
+        # Initialize Actor and Critic
+        self.actor = policy_class(self.obs_dim, self.sen_dim, self.out_ac1_dim, self.out_ac2_dim, self.seq_len)
+        self.critic = value_class(self.obs_dim, 1, self.seq_len)
+        # Initialize optimizers for Actor and Critic
+        self.actor_optim  = Adam(self.actor.parameters(), lr= self.lr)
+        self.critic_optim = Adam(self.critic.parameters(), lr=self.lr)
+                
+        # This logger will help for printing the summary
+        self.logger = {
+            'delta_t': time.time_ns(),
+            't_so_far': 0,
+            'i_so_far': 0, 
+            'batch_lens': [],
+            'batch_rews': [],
+            'actord_losses': [],
+            'actorc_losses': [],
+        }
+        
+    def learn(self, total_timesteps):
+        print(f"Learning...Running {self.max_timesteps_per_episode} timesteps per episode, ", end='')
+        print(f"{self.timesteps_per_batch} timesteps per batch for a total of {total_timesteps} timesteps")
+        t_so_far = 0
+        i_so_far = 0
+        while t_so_far < total_timesteps:
+
+            t_it = total_timesteps / self.timesteps_per_batch                        # Number of total iterations in this training
+
+            #Variance annealing
+            var =  self.var * (1e-5/self.var)**(i_so_far/t_it)                       # initial * (final/initial)***(t/T)         
+            self.cov_var = torch.full(size=(self.out_ac2_dim,),fill_value=var)      
+            self.cov_mat = torch.diag(self.cov_var)                                 
+
+            #Learning rate annealing
+            new_lr = self.lr * (1e-6/self.lr)**(i_so_far/t_it)                       # initial * (final/initial)***(t/T) 
+            self.actor_optim.param_groups[0]["lr"] = new_lr
+            self.critic_optim.param_groups[0]["lr"] = new_lr
+
+            #Temperature annealing
+            self.temperature = self.temp * (0.001/self.temp)**(i_so_far/t_it)                 # initial * (final/initial)***(t/T)
+
+            #Entropy annealing
+            self.ent_coeff = self.ent_init * (0.00001/self.ent_init)**(i_so_far/t_it)   # initial * (final/initial)***(t/T) 
+
+            #Calling Rollout
+            batch_obs, batch_mask_vec, batch_dacts, batch_cacts, batch_log_probsd, batch_log_probsc, batch_lens, batch_h, batch_c, batch_vals, batch_rews, done_flags = self.rollout() 
+            t_so_far += np.sum(batch_lens)
+            i_so_far += 1
+            self.logger['t_so_far'] = t_so_far
+            self.logger['i_so_far'] = i_so_far
+
+            batch_h = batch_h.detach()          # Consider them as constants so the backpropagation does not alter them while updating
+            batch_c = batch_c.detach()          # Consider them as constants so the backpropagation does not alter them while updating
+
+            #Normalize the obs by dividing by max values
+            batch_obs_n = np.divide(batch_obs, self.max_vals_obs)
+            batch_obs_n = batch_obs_n.reshape(-1,self.seq_len,self.obs_dim)
+
+            V = self.evaluate_V(batch_obs_n, batch_h, batch_c)
+
+            # For minibatch construction
+            b_size = batch_obs_n.size(0)
+            idxs = np.arange(b_size)
+            mini_batchsize = b_size//self.num_minibatches
+
+            # Generalized Advantage Estimation function
+            A_k = self.GAE(batch_rews, batch_vals, done_flags)
+            V = V.squeeze()
+            batch_rtgs = A_k + V.detach()
+
+            # For a more stable convergence
+            A_k = (A_k - A_k.mean()) / (A_k.std() + 1e-10)
+
+            # Loop for updating the network n number of epochs
+            for _ in range(self.n_updates_per_iteration):
+                np.random.shuffle(idxs)
+                for start in range (0, b_size, mini_batchsize):  #Making minibatches for updating the networks.
+                    end = start + mini_batchsize
+                    id = idxs[start:end]
+                    min_batch_obs_n         = batch_obs_n[id,:,:]
+                    min_batch_mask_vec      = batch_mask_vec[id,:]
+                    min_batch_dacts         = batch_dacts[id,:]
+                    min_batch_cacts         = batch_cacts[id,:,:]
+                    min_batch_log_probsd    = batch_log_probsd[id]
+                    min_batch_log_probsc    = batch_log_probsc[id]
+                    min_batch_rtgs          = batch_rtgs[id]
+                    min_batch_h             = batch_h[:,id,:]
+                    min_batch_c             = batch_c[:,id,:]
+                    min_A_k                 = A_k[id]
+
+                    # Update Continuous Agent
+                    curr_log_probsc, entropyc = self.evaluate_c(min_batch_obs_n, min_batch_mask_vec, min_batch_cacts, min_batch_h, min_batch_c)
+                    logratios_c = curr_log_probsc - min_batch_log_probsc
+                    ratioc = torch.exp(logratios_c)
+                    surr1c = ratioc * min_A_k
+                    surr2c = torch.clamp(ratioc, 1-self.clip, 1+self.clip) * min_A_k
+                    actorc_loss = (-torch.min(surr1c, surr2c)).mean() - entropyc.mean() * self.ent_coeff
+                    #gp = open('actorc_loss','a')
+                    #writer = csv.writer(gp, lineterminator='\n')
+                    #writer.writerow([np.array(actorc_loss.detach())])
+                    #gp.close()
+                    self.logger['actorc_losses'].append(actorc_loss.detach())
+                    #Code to see the names of the layers
+                    #for name, param in self.actor.named_parameters(): 
+                    #    if param.requires_grad: 
+                    #        print(name,param.data.shape)
+                    #for i, param in enumerate(self.actor.parameters()):
+                    #     print(i,param.data.shape)
+                    # Deactivate the gradients from the Discrete Actor
+                    for i, param in enumerate(self.actor.parameters()):
+                        if i in [16,17,18,19,20,21]:
+                            param.requires_grad = False
+                    # Optimize the whole network (with the frozen params)
+                    self.actor_optim.zero_grad()
+                    actorc_loss.backward(retain_graph=True)
+                    nn.utils.clip_grad_norm_(self.actor.parameters(), self.max_grad_norm)
+                    self.actor_optim.step()
+                    # Reactivate Everything
+                    for i, param in enumerate(self.actor.parameters()):
+                        param.requires_grad = True
+
+                    # Update Discrete Agent
+                    curr_log_probsd, entropyd = self.evaluate_d(min_batch_obs_n, min_batch_mask_vec, min_batch_dacts, min_batch_h, min_batch_c)
+                    logratios_d = curr_log_probsd - min_batch_log_probsd
+                    ratiod = torch.exp(logratios_d)
+                    surr1d = ratiod * min_A_k
+                    surr2d = torch.clamp(ratiod, 1-self.clip, 1+self.clip) * min_A_k
+                    actord_loss = (-torch.min(surr1d, surr2d)).mean() - entropyd.mean() * self.ent_coeff
+                    #gl = open('actord_loss','a')
+                    #writer = csv.writer(gl, lineterminator='\n')
+                    #writer.writerow([np.array(actord_loss.detach())])
+                    #gl.close()
+                    self.logger['actord_losses'].append(actord_loss.detach())
+                    # Deactivate the gradients from the Continuous Actor
+                    for i, param in enumerate(self.actor.parameters()):
+                        if i in [22,23,24,25,26,27]:
+                            param.requires_grad = False
+                    # Optimize the whole network (with the frozen params)
+                    self.actor_optim.zero_grad()
+                    actord_loss.backward(retain_graph=True)
+                    nn.utils.clip_grad_norm_(self.actor.parameters(), self.max_grad_norm)
+                    self.actor_optim.step()
+                    # Reactivate everything
+                    for i, param in enumerate(self.actor.parameters()):
+                        param.requires_grad = True
+                    
+                    # Update the Critic 
+                    V = self.evaluate_V(min_batch_obs_n, min_batch_h, min_batch_c)
+                    critic_loss = nn.MSELoss()(V, min_batch_rtgs)
+                    self.critic_optim.zero_grad()
+                    critic_loss.backward()
+                    nn.utils.clip_grad_norm_(self.critic.parameters(), self.max_grad_norm)
+                    self.critic_optim.step()
+                
+            # Print a summary
+            self._log_summary()
+            # Save the model if it's time
+            if i_so_far % self.save_freq == 0:
+                torch.save(self.actor.state_dict(),  './ppo_actor_exp2.pth')
+                torch.save(self.critic.state_dict(), './ppo_critic_exp2.pth')  
+
+    def rollout(self):
+        batch_obs =         []
+        batch_dacts =       []
+        batch_cacts =       []
+        batch_log_probsd =  []
+        batch_log_probsc =  []
+        batch_rews =        []
+        batch_lens =        []
+        batch_mask_vec =    []
+        done_flags =        []
+        batch_vals =        []
+        batch_h =           []
+        batch_c =           []
+        t = 0
+        # Episode run
+        while t < self.timesteps_per_batch:
+            ep_rews = []
+            ep_vals = []
+            ep_dones= []
+            done = False
+            times = [0,0,0,0,0,0,0,0,0]
+            prev = torch.zeros([self.seq_len,self.obs_size])          # CREATE OBS SEQUENCE
+            mask_vec = self.env.reset_m(prev[-1])
+            h, c = (torch.zeros((3,1,25)),torch.zeros((3,1,25)))      #LAYERS, BATCHES, HIDDEN SIZE
+            for ep_t in range(self.max_timesteps_per_episode):    
+                t += 1      
+                
+                percentage = 0
+                zeros = int(percentage*self.obs_size)
+                ones = self.obs_size - zeros
+                part_o1 = np.array(([0]*zeros + [1]*ones), dtype=np.float32) 
+                np.random.shuffle(part_o1)
+                prev_m = torch.clone(prev)
+                if prev_m[-1][-1] % 2 ==0: 
+                    prev_m[-1] = prev_m[-1]*part_o1
+
+                batch_obs.append(torch.clone(prev_m))    #prev_m 
+                batch_mask_vec.append(mask_vec)
+                batch_h.append(h)
+                batch_c.append(c)
+                daction, caction, log_probd, log_probc, h1, c1 = self.get_action(prev_m, mask_vec, h, c)  #prev_m
+                val = self.critic(prev_m, h, c)          #prev_m
+                h = h1
+                c = c1
+                call = torch.clone(prev[-1])
+                obs1, rew, done, times, mask_vec = self.env.step1(daction, caction, call, times) 
+                ep_dones.append(done)
+                obs1 = obs1.reshape([1,self.obs_size])
+                prev = torch.cat((prev_m,obs1),0)        #prev_m
+                prev = prev[1:self.seq_len+1]
+                ep_rews.append(rew)
+                ep_vals.append(val.flatten())
+                batch_dacts.append(daction)
+                batch_cacts.append(caction)
+                batch_log_probsd.append(log_probd)
+                batch_log_probsc.append(log_probc) 
+                if done:
+                    break
+            batch_vals.append(ep_vals)
+            done_flags.append(ep_dones)
+            batch_lens.append(ep_t+1)
+            batch_rews.append(ep_rews)
+        # Reshape data as tensors in the appropiate shap        
+        batch_h         = torch.concat(batch_h, dim=1)
+        batch_c         = torch.concat(batch_c, dim=1)  
+        batch_obs       = torch.concat(batch_obs)
+        batch_mask_vec  = np.array(batch_mask_vec)
+        batch_mask_vec  = torch.tensor(batch_mask_vec, dtype=bool)
+        batch_dacts     = np.array(batch_dacts)
+        batch_dacts     = torch.tensor(batch_dacts, dtype=torch.int)
+        batch_cacts     = np.array(batch_cacts)
+        batch_cacts     = torch.tensor(batch_cacts, dtype=torch.float)
+        batch_log_probsd = torch.tensor(batch_log_probsd, dtype=torch.float)
+        batch_log_probsc = torch.tensor(batch_log_probsc, dtype=torch.float)
+        #batch_rtgs = self.compute_rtgs(batch_rews)
+        # Save the episodic returns and lenghts
+        self.logger['batch_rews'] = batch_rews
+        self.logger['batch_lens'] = batch_lens
+        return batch_obs, batch_mask_vec, batch_dacts, batch_cacts, batch_log_probsd, batch_log_probsc, batch_lens, batch_h, batch_c, batch_vals, batch_rews, done_flags
+        
+    def GAE(self, rewards, values, dones):
+        advs = []
+        for ep_rews, ep_vals, ep_done in zip (rewards,values,dones):
+            advantages = []
+            last_adv = 0
+            for t in reversed(range(len(ep_rews))):
+                if t + 1 < len(ep_rews):
+                    delta = ep_rews[t] + self.gamma * ep_vals[t+1] * (1 - ep_done[t+1]) - ep_vals[t]
+                else:
+                    delta = ep_rews[t] - ep_vals[t]
+
+                advantage = delta + self.gamma * self.lam * (1-ep_done[t])*last_adv
+                last_adv = advantage
+                advantages.insert(0,advantage)
+            advs.extend(advantages)
+        return torch.tensor(advs, dtype=torch.float)
+    
+    def get_action(self, obs2, mask_vec, h, c):
+        # Normalize the obs by dividing by max values
+        obs_n = np.divide(obs2,self.max_vals_obs)  
+        # Get action
+        act_d, act_c, h, c = self.actor(obs_n, mask_vec, h, c)
+        # Apply temperature
+        act_d = act_d/self.temperature
+        act_d = F.softmax(act_d,dim=-1)
+        cat = Categorical(act_d)
+        dist = MultivariateNormal(act_c, self.cov_mat)
+        # Sample an action from the distributions
+        action_d = cat.sample()
+        action_c = dist.sample()
+        # Calculate the log probability
+        log_prob_d = cat.log_prob(action_d)
+        log_prob_c = dist.log_prob(action_c)
+        # Detach and make np arrays
+        action_d = action_d.detach().numpy()
+        action_c = action_c.detach().numpy()
+        log_prob_d = log_prob_d.detach()
+        log_prob_c = log_prob_c.detach()
+        return action_d, action_c, log_prob_d, log_prob_c, h, c
+    
+    def evaluate_V(self, batch_obsv, batch_h, batch_c):
+        V = self.critic(batch_obsv, batch_h, batch_c).squeeze()
+        return V
+    
+    def evaluate_d(self, batch_obsd, batch_mask_vec, batch_dacts, batch_h, batch_c):
+        act_d, _, _, _ = self.actor(batch_obsd, batch_mask_vec, batch_h, batch_c)
+        act_d = act_d/self.temperature    
+        act_d = F.softmax(act_d,dim=-1)  
+        meand = torch.tensor([])
+        mean_en = torch.tensor([])
+        for i in range(int(len(batch_obsd))):
+            cat = Categorical(act_d[i])
+            meand = torch.cat((meand,cat.log_prob(batch_dacts[i])))
+            cat_ent = torch.tensor([cat.entropy().detach()])
+            mean_en = torch.cat((mean_en, cat_ent))     
+        return meand, mean_en
+        
+    def evaluate_c(self, batch_obsc, batch_mask_vec, batch_cacts, batch_h, batch_c):
+        _ , act_c, _, _= self.actor(batch_obsc, batch_mask_vec, batch_h, batch_c)
+        batch_cacts_s = batch_cacts.squeeze()
+        dist = MultivariateNormal(act_c, self.cov_mat)
+        meanc = dist.log_prob(batch_cacts_s)
+        ent_c = dist.entropy()                         
+        return meanc, ent_c
+    
+    def _init_hyperparameters(self, hyperparameters):
+        self.timesteps_per_batch =      620
+        self.seq_len =                  4           
+        self.obs_size =                 15
+        self.max_steps_per_episodes =   31
+        self.n_updates_per_iteration =  10
+        self.lr =                       1e-4
+        self.gamma =                    0.99
+        self.clip =                     0.2
+        self.save_freq =                10
+        self.seed =                     None        
+        self.var =                      1e-2
+        self.temp =                     1
+        self.ent_coeff=                 0.20
+        self.num_minibatches=           5
+        self.max_grad_norm=             0.5
+        self.lam=                       0.98
+        self.target_kl =                0.02 
+
+        for param, val in hyperparameters.items():
+            exec('self.' + param + '=' + str(val))
+
+        if self.seed != None:
+            assert(type(self.seed) == int)
+            torch.manual_seed(self.seed)
+            print(f'Succesfully set used to {self.seed}')
+    
+    def _log_summary(self):
+        delta_t = self.logger['delta_t']
+        self.logger['delta_t'] = time.time_ns()
+        delta_t = (self.logger['delta_t'] - delta_t) / 1e9
+        delta_t = str(round(delta_t,2))
+        
+        t_so_far = self.logger['t_so_far']
+        i_so_far = self.logger['i_so_far']
+        avg_ep_lens = np.mean(self.logger['batch_lens'])
+        avg_ep_rews2 = np.mean([np.sum(ep_rews) for ep_rews in self.logger['batch_rews']])
+        avg_actord_loss = np.mean([losses.float().mean() for losses in self.logger['actord_losses']])
+        avg_actorc_loss = np.mean([losses.float().mean() for losses in self.logger['actorc_losses']])
+        
+        # Rounding to decimal places
+        avg_ep_lens = str(round(avg_ep_lens, 2))
+        avg_ep_rews = str(round(avg_ep_rews2, 2))       
+        avg_actord_loss = str(round(avg_actord_loss, 5))
+        avg_actorc_loss = str(round(avg_actorc_loss, 5))
+        
+        # Save values of rewards
+        avg_ep_rews1 = (round(avg_ep_rews2,2))
+        f = open('rewards_exp2', 'a')
+        writer = csv.writer(f, lineterminator = '\n')
+        writer.writerow([avg_ep_rews1])
+        f.close()
+        
+        # Print 
+        print(flush=True)
+        print(f"----------------- Iteration #{i_so_far}-----------------", flush=True)
+        print(f"Average Episodic Length: {avg_ep_lens}", flush=True)
+        print(f"Average Episodic Return: {avg_ep_rews}", flush=True)
+        print(f"Average Discrete Loss: {avg_actord_loss}", flush=True) 
+        print(f"Average Continuous Loss: {avg_actorc_loss}", flush=True)
+        print(f"Timesteps so Far: {t_so_far}", flush=True)
+        print(f"Iteration took: {delta_t}", flush=True)
+        print(f"--------------------------------------------------------", flush=True)
+        print(flush=True)
+        
+        # Reset batch-specific logging data
+        self.logger['batch_lens'] = []
+        self.logger['batch_rews'] = []
+        self.logger['actord_losses'] = []
+        self.logger['actorc_losses'] = []
